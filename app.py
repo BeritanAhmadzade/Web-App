8201910be82c5000c59577dcf4df2f4ea76229b5
BeritanAhmadzade <beritanahmadzade@gmail.com>
2025-11-03 11:46:42 +0330

    fix app port based on the nginx config

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask,render_template
-from threading import Thread
-app=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
-def run_app():
-    app.run(host='127.0.0.1',port=5000,debug=True,use_reloader=False)
 
-thread=Thread(target=run_app)
-thread.start()
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5001, debug=False)
